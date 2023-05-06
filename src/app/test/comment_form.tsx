43254a3a39e1ed239994f989821633738b5b15df
Masanarea <93495976+Masanarea@users.noreply.github.com>
2023-05-06 11:35:02 +0900

    2023年05月新規開発 | 基本動作に問題なし(クライアント側でAPI Key が見えてしまうので早急に対応)

--- src/app/test/comment_form.tsx
+++ src/app/test/comment_form.tsx
@@ -1,22 +1,116 @@
 'use client'
-import { useState } from 'react'
-// import axios from 'axios'
+import { GetStaticProps } from 'next'
+import { useState, useEffect } from 'react'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
 
-export const UserPage: React.FC = () => {
+const openaiApiKey: string | undefined = process.env.NEXT_PUBLIC_OPENAI_API_KEY
+
+type GenerateCommentRequestData = {
+  premise: string
+}
+
+type GenerateCommentResponseData = {
+  comment: string
+}
+
+export const UserPage = () => {
+  async function fetchSentences() {
+    // const response = await fetch("https://api-for-datumou-app.vercel.app/getSentenceList?limit=50");
+    const response = await fetch(
+      'https://api-for-datumou-app.vercel.app/getSentenceList?limit=1'
+    )
+    // const response = await fetch("https://api-for-datumou-app.vercel.app/getSentenceList?limit=5");
+    // const response = await fetch("https://api-for-datumou-app.vercel.app/getSentenceList?limit=10");
+    const data = await response.json()
+    console.log('Working444')
+    console.log(data.response_data)
+
+    return data.response_data
+  }
+
+  const [sentences, setSentences] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const sentences = await fetchSentences()
+      setSentences(sentences)
+      console.log('Working')
+      console.log(sentences)
+    }
+    console.log('Working121')
+
+    fetchData()
+  }, [])
+
+  interface UserComment {
+    text: string
+  }
+
+  const [userComment, setUserComment] = useState(
+    '実装内容を確認している最中となります。また確認が終わり次第ご連絡させていただき、cst_tinker側の案件は当分は作業内容はありませんので順次もう１つの案件の作業に着手していければと考えております。'
+  )
+  const [generatedComment, setGeneratedComment] = useState<string>('')
+
+  // console.log(openaiApiKey)
   const [comments, setComments] = useState([])
-  // const [content, setContent] = useState('')
   const [comment, setComment] = useState('')
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+  // return;
 
-    // ユーザーの前提条件を使用して、コメントを生成するためのAPIリクエストを送信する
-    // const response = await axios.post('/api/generate-comment', { comment });
-    // const response = await axios.post('/api/generate-comment', { comment });
+  const handleGenerateComment = async () => {
+    setGeneratedComment('検索結果\nLoading...')
+    const referenceDataLists: string[] = sentences
+    const referenceSentence: string = referenceDataLists.join('\n\n')
+    // console.log('APIデータの内容')
+    // console.log(sentences)
+    // console.log('参考にする資料');
+    // console.log(referenceSentence);
+    const prompt = `サンプルを参考にして、以下のユーザーが記述したコメントをより読み手に明確に内容が伝わるように修正して下さい\n\n ユーザーが記述したコメント : 『 ${comment} 』 \n\n サンプル : 『 ${referenceSentence} 』`
+    console.log('プロンプトの内容')
+    console.log(prompt)
 
-    // APIから返されたコメントを表示する
-    console.log('OK!!')
-    // e.target.reset()
+    try {
+      const API_URL = 'https://api.openai.com/v1/'
+      const MODEL = 'gpt-3.5-turbo'
+      const response = await axios.post(
+        `${API_URL}chat/completions`,
+        {
+          // モデル ID の指定
+          model: MODEL,
+          // 質問内容
+          messages: [
+            {
+              role: 'user',
+              content: prompt,
+            },
+          ],
+        },
+        {
+          // 送信する HTTP ヘッダー(認証情報)
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${openaiApiKey}`,
+          },
+        }
+      )
+      // 回答の取得
+      const generatedText: string = response.data.choices[0].message.content
+
+      setGeneratedComment(generatedText)
+      console.log('レスポンス')
+      console.log(response)
+      console.log('生成されたコメント')
+      console.log(generatedComment)
+      console.log('入力値(生成されたコメント)')
+      console.log(generatedText)
+    } catch (error) {
+      console.error(error)
+      return null
+    }
+
+    // setGeneratedComment({ text: generatedText });
+    // console.log(generatedComment)
   }
 
   return (
@@ -25,32 +119,33 @@ export const UserPage: React.FC = () => {
         <h1 className="text-3xl font-extrabold tracking-tight text-gray-900">
           ユーザーページ
         </h1>
-        <form onSubmit={handleSubmit} className="mt-6 space-y-6">
-          <div className="flex flex-wrap mb-6">
-            <label
-              htmlFor="comment"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              コメントを入力してください
-            </label>
-            <textarea
-              name="comment"
-              id="comment"
-              className="bg-white border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              rows="6"
-              placeholder="ここにコメントを入力"
-              onChange={(event) => setComment(event.target.value)}
-              value={comment}
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+        {/* <form  className="mt-6 space-y-6"> */}
+        <div className="flex flex-wrap mb-6">
+          <label
+            htmlFor="comment"
+            className="block text-gray-700 text-sm font-bold mb-2"
           >
-            送信
-          </button>
-        </form>
+            コメントを入力してください
+          </label>
+          <textarea
+            name="comment"
+            id="comment"
+            className="bg-white border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            rows="6"
+            placeholder="ここにコメントを入力"
+            onChange={(event) => setComment(event.target.value)}
+            value={comment}
+            required
+          />
+        </div>
+        <button
+          onClick={handleGenerateComment}
+          className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+        >
+          送信
+        </button>
+        <p>{generatedComment}</p>
+        {/* </form> */}
         <div className="mt-6 space-y-6">
           {comments.map((comment, index) => (
             <div key={index} className="comment-container">
