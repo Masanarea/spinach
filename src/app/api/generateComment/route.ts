8112a92d06ffd59e01231a3853d4e7c7dfd8cb99
Masanarea <93495976+Masanarea@users.noreply.github.com>
2023-05-16 23:27:56 +0900

    2023年05月新規開発 | プロンプトに対する返答のtest4

--- src/app/api/generateComment/route.ts
+++ src/app/api/generateComment/route.ts
@@ -19,7 +19,7 @@ export async function POST(request: Request): Promise<NextResponse> {
     const API_URL = 'https://api.openai.com/v1/'
     const MODEL = 'gpt-3.5-turbo'
     const referenceSentence = refComments.join('\n\n')
-    const prompt = `ユーザーが記述したコメントをより読みやすく、明確に伝わるように修正してください。\n\n ユーザーが記述したコメント : 『 ${premise} 』 \n\n サンプルを参考に修正を行ってください。サンプル : 『 ${referenceSentence} 』`
+    const prompt = `ユーザーが記述したコメントである『 ${premise} 』をより読みやすく、明確に伝わるようにサンプルを参考に修正を行ってください \n\n サンプル : 『 ${referenceSentence} 』`
     const response = await axios.post(
       `${API_URL}chat/completions`,
       {
